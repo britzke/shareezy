b78d10c47a8b8aa74916c3f830531a02cd0ea5da
ThomasKlawitter <Thomas.Klawitter@web.de>
2013-12-05 10:39:34 +0100

    Meine Bean für neue Ressourcen

---
+++ neueRessourceBean.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+
+/**
+ * @author e1_klawitter
+ *Die Annotation ManagedBean sorgt dafür, dass diese classe als MBean erkannt wird
+ *SessionScoped sorgt dafür das die Bean nur bis zum ende der Session lebt
+ */
+@ManagedBean
+@SessionScoped
+
+public class neueRessourceBean {
+	
+	
+
+	
+	
+	public neueRessourceBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	/**
+	 * neueRessource() ist die Actionmethode die eine BlankoRessource listet in die mit Eigenschaften zu füllen ist 
+	 */
+	private void neueRessource()
+	{		
+	}
+	
+	/**
+	 * loescheRessource() sorgt dafür das Resourcen aus der Ressourcenliste gelöscht werden können
+	 */
+	private loescheRessource()
+	{
+		
+	}
+	
+	/**
+	 * speichern() ist dafür da die nun eingetragenen Eigenschaften der neu angelegten Ressource zu speichern
+	 */
+	
+	private void speichern()
+	{
+		
+	}
+	
+	
+	
+	
+
+}
