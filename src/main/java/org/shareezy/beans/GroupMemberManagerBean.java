beed2ee769e8d4de0ef8d559da96b8c888115e8e
tmocakeling <timo.kuchling@web.de>
2013-12-05 09:12:22 +0100

    Updated GroupMemberManagerBean

--- src/main/java/org/shareezy/beans/GroupMemberManagerBean.java
+++ src/main/java/org/shareezy/beans/GroupMemberManagerBean.java
@@ -1,11 +1,35 @@
 package org.shareezy.beans;
 
-//@ManagedBean(name="MemberManager")
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="MemberManager")
 public class GroupMemberManagerBean {
 	
 	public GroupMemberManagerBean(){
 		
 	}
-
-
+	
+	/** Die Methode AddUser dient dazu dem Benutzer eine Gruppe zuzuweisen
+	 * 
+	 *  - wird bei Klick auf 'Hinzufügen' und 'Annehmen' aufgerufen
+	 *  - erstellt die Zuweisung in der AccountsGruppen Entität
+	 */
+	public void addUser(){ 
+		
+		}
+	
+	/** Die Methode deleteUser dient dazu die Gruppenzuweisung eines Benutzers zu entfernen
+	 * 
+	 *  - wird bei Klick auf 'Entfernen' aufgerufen
+	 *  - löscht die Zuweisung in der AccountsGruppen Entität
+	 */
+	public void deleteUser(){ 
+		}
+	
+	/** Die Methode deleteRequest dient dazu eine Anfrage in der Gruppendetailansicht zu entfernen
+	 * 
+	 *  - wird bei Klick auf Ablehnen aufgerufen
+	 */
+	public void deleteRequest(){ 
+	}
 }
