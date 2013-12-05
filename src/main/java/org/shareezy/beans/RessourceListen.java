37ddc9e66afea91dff71a2eaeea1ace4727b72d1
timtreibmann <melusinesportal@yahoo.de>
2013-12-05 09:31:23 +0100

    #4 Erstellung der Klasse RessourceListen mit einer Actionmethode

---
+++ src/main/java/org/shareezy/beans/RessourceListen.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.shareezy.beans;
+
+/**
+ * Diese Klasse stellt die Eigenschaften und Funktinen für das Listen von Ressourcen zur Verfügung.
+ * Ressourcen werden in der Ressourcenansicht.xhtml, EigeneRessourcen.xhtml, Gruppenverwaltung.xhtml und eigeneBuchungen.xhtml
+ * gelistet.
+ * 
+ * @author e1_treibmann
+ *
+ */
+public class RessourceListen {
+	/**
+	 * Die Methode ressourceClicked leitet bei einem Klick auf den Namen der Ressource, den Benutzer auf die Detailansicht der Ressource weiter.
+	 * @return null
+	 */
+	public String ressourceClicked(){
+	return null;
+	}
+
+}
