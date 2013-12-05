7cbb189d025e6a81b4649f87526127edd60cb973
e1_hermann <e1_hermann@EDV003>
2013-12-05 10:23:03 +0100

    geändert

---
+++ src/org/shareezy/beans/Gruppenzuordnung.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.shareezy.beans;
+
+import java.awt.Button;
+
+/**
+ * @author e1_hermann
+ *
+ */
+public class Gruppenzuordnung {
+	
+	Button button;
+	
+	
+	
+	public String  gruppentfernen(){
+		return null;
+		// Mitglied aus der Gruppe entfernen
+		// gruppenid
+	}	
+	public String mitgliederabfragen(){
+		return null;
+		// welches Mitglied ist berechtigt zum abfragen/erstellen/verwalten von Ressourcen
+	}
+	public String ressourcenid(){
+		return null;
+		// ressource zur gruppe hinzufügen
+	}
+    public String ressourceedit(){
+    	return null;
+    	// bearbeiten der ressource
+    }
+	
+}
+	
+
+
