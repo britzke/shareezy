3c8d0e61f19606f8e1c49fd42e4225a0d3592f06
e1_hermann <e1_hermann@EDV003>
2013-12-05 10:56:22 +0100

    #7 Funktion "Gruppenzuodnung von Resourcen erstellen/verwalten"

--- src/org/shareezy/beans/Gruppenzuordnung.java
+++ src/org/shareezy/beans/Gruppenzuordnung.java
@@ -1,37 +1,54 @@
 /**
- * 
- */
+ *Es wird geprüft welche Mitglieder Ressourcen inerhalb der Gruppe verwalten können.mitglieder?
+ *Wenn jemand aus der Gruppe entfernt wird/geht
+ *Welche Ressource zur Gruppe hinzugefügt wird
+ *Nachträgliches bearbeiten der ressourcen zu gruppe/ID
+ *Ressource der gruppe hinzufügen
+ **/
 package org.shareezy.beans;
 
-import java.awt.Button;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
-/**
- * @author e1_hermann
- *
- */
+@ManagedBean
+@SessionScoped
 public class Gruppenzuordnung {
 	
-	Button button;
+	public String gruppe;
+	public String mitglied;
 	
-	
-	
+	/**
+	 * Mitglied aus der Gruppe entfernen/gruppenID
+	 * @return
+	 */
 	public String  gruppentfernen(){
 		return null;
-		// Mitglied aus der Gruppe entfernen
-		// gruppenid
-	}	
+		}
+	
+	/**
+	 * welches Mitglied ist berechtigt zum abfragen/erstellen/verwalten von Ressourcen
+	 * @return
+	 */
 	public String mitgliederabfragen(){
 		return null;
-		// welches Mitglied ist berechtigt zum abfragen/erstellen/verwalten von Ressourcen
 	}
+	
+	/**	
+	 * ressource zur gruppe hinzufügen
+	 * @return
+	 */
 	public String ressourcenid(){
 		return null;
-		// ressource zur gruppe hinzufügen
 	}
+	
+	/**
+	 * bearbeiten der ressource
+	 * @return
+	 */
     public String ressourceedit(){
     	return null;
-    	// bearbeiten der ressource
     }
+    
 }
 	
 
