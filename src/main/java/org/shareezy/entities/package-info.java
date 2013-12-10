e667a705effd2c187ddd4299a352d0b4f582e2cf
britzke <britzke@lette-verein.de>
2013-12-10 11:45:45 +0100

    Autorenschaft vervollständigt

--- src/main/java/org/shareezy/entities/package-info.java
+++ src/main/java/org/shareezy/entities/package-info.java
@@ -1,6 +1,6 @@
 
 /**
  * Die Klassen in diesem Paket dienen der objektrelationalen Abbildung (ORM).
- * @author britzke (bubi@charmides.in-berlin.de)
+ * @author burghard.britzke (bubi@charmides.in-berlin.de)
  */
 package org.shareezy.entities;
