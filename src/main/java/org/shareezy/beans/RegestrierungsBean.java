c5f463ce0bc0ac13f42ae7682280778e4cebd61f
Engelskirchen <wannyalchemic@gmail.com>
2013-12-10 09:49:36 +0100

    Hinzufügen der parameter in der AccountBearbeitenBean und der RegestrierungsBean

--- src/main/java/org/shareezy/beans/RegestrierungsBean.java
+++ src/main/java/org/shareezy/beans/RegestrierungsBean.java
@@ -17,24 +17,63 @@ public class RegestrierungsBean {
 	 * Prüft ob die Spezifischen Daten(username, e-mail) schon in einem
 	 * Datansatz vorhanden sind. Sind die Spezifischen Daten noch nicht
 	 * vorhanden, wird die Methode datensatzEinfügen() aufgerufen.
+	 * 
+	 * @param name
+	 *            Der Inhalt des textfeldes Nachname.
+	 * @param vorname
+	 *            Der Inhalt des textfeldes Vorname.
+	 * @param username
+	 *            Der Inhalt des textfeldes username.
+	 * @param eMail
+	 *            Der Inhalt des textfeldes e-Mail.
+	 * @param passwort
+	 *            Der Inhalt des textfeldes passwort.
+	 * @param passwortWiederholen
+	 *            Der Inhalt des textfeldes passwort wiederholen.
 	 */
-	public void datensatzPrüfen() {
+	public void datensatzPrüfen(String name, String vorname, String username,
+			String eMail, String passwort, String passwortWiederholen) {
 
 	}
 
 	/**
 	 * Fügt der Entität "Benutzer" der Datenbank einen neuen datensatz mit den
-	 * vom Benutzer eingegebenen Daten hinzu.
+	 * vom Benutzer eingegebenen Daten hinzu. Danach wird die Methode
+	 * dalidierungsEmail() aufgerufen.
+	 * 
+	 * @param name
+	 *            Der Inhalt des textfeldes Nachname, den die Methode von
+	 *            dantensatzPrüfen() übergeben bekommt.
+	 * @param vorname
+	 *            Der Inhalt des textfeldes Vornameden, die Methode von
+	 *            dantensatzPrüfen() übergeben bekommt.
+	 * @param username
+	 *            Der Inhalt des textfeldes username, den die Methode von
+	 *            dantensatzPrüfen() übergeben bekommt.
+	 * @param eMail
+	 *            Der Inhalt des textfeldes e-Mail, den die Methode von
+	 *            dantensatzPrüfen() übergeben bekommt.
+	 * @param passwort
+	 *            Der Inhalt des textfeldes passwort, den die Methode von
+	 *            dantensatzPrüen() übergeben bekommt.
+	 * @param passwortWiederholen
+	 *            Der Inhalt des textfeldes passwort wiederholen, den die Methode
+	 *            von dantensatzPrüfen() übergeben bekommt.
 	 */
-	public void datensatzEinfügen() {
+	public void datensatzEinfügen(String name, String vorname, String username,
+			String eMail, String passwort, String passwortWiederholen) {
 
 	}
 
 	/**
 	 * Schickt bei Erfolgreicher Regestrierung eine bestätigungs e-mail an den
 	 * Benutzer.
+	 * 
+	 * @param eMail
+	 *            die e-mail Addresse des Benutzers, den den die Methode von
+	 *            dantensatzEinfügen() übergeben bekommt.
 	 */
-	public void validierungsEmail() {
+	public void validierungsEmail(String eMail) {
 
 	}
 
