b155d8958641cb024a627e7c5510141866546b54
burghard.britzke <bubi@charmides.in-berlin.de>
2013-12-17 21:58:03 +0100

    # #2 GPL Header eingefügt

--- src/test/java/org/shareezy/test/unit/RegistrierungBeanTest.java
+++ src/test/java/org/shareezy/test/unit/RegistrierungBeanTest.java
@@ -1,5 +1,21 @@
-/**
- * 
+/*
+ * This file is part of shareezy, a software system for sharing resources.
+ *
+ * Copyright (C) 2013  	e1_cakir,
+ * 						Maurice Engelskirchen
+ * 						burghard.britzke (bubi@charmides.in-berlin.de)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.shareezy.test.unit;
 
