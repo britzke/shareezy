b10613f2c6bcc0487b721f24189b54cb9c6194ea
burghard.britzke <bubi@charmides.in-berlin.de>
2014-01-05 14:31:21 +0100

    #2 Umstellung auf Projektspezifische Init-Parameter, um Konflikte mit anderen Projekten zu vermeiden, sowie User und Word werden nur gesetzt, wenn "MAIL_SMTP_AUTH" den Wert "true" hat.

--- src/main/java/org/shareezy/ContextListener.java
+++ src/main/java/org/shareezy/ContextListener.java
@@ -21,22 +21,26 @@ public class ContextListener implements ServletContextListener {
 	 * <dt>org.shareezy.MAIL_FROM</dt>
 	 * <dd>mail.from - E-Mailadresse des Absenders</dd>
 	 * <dt>org.shareezy.MAIL_SMTP_STARTTLS</dt>
-	 * <dd>mail.smtp.starttls - Gibt an, ob die Authentisierung mit dem SMTP-Server mittels TLS
-	 * durchgeführt wird</dd>
+	 * <dd>mail.smtp.starttls - Gibt an, ob die Authentisierung mit dem
+	 * SMTP-Server mittels TLS durchgeführt wird</dd>
 	 * <dt>org.shareezy.MAIL_SMTP_AUTH</dt>
-	 * <dd>mail.smtp.auth - Gibt an, ob eine Authentisierung mit dem SMTP-Server durchgeführt werden muss</dd>
+	 * <dd>mail.smtp.auth - Gibt an, ob eine Authentisierung mit dem SMTP-Server
+	 * durchgeführt werden muss</dd>
 	 * <dt>org.shareezy.MAIL_SMTP_HOST</dt>
 	 * <dd>mail.smtp.host - Name des SMTP-Mailservers</dd>
 	 * <dt>org.shareezy.MAIL_SMTP_PORT</dt>
-	 * <dd>mail.smtp.port - Port, an dem der SMTP-Service zur Verfügung gestellt wird</dd>
+	 * <dd>mail.smtp.port - Port, an dem der SMTP-Service zur Verfügung gestellt
+	 * wird</dd>
 	 * <dt>org.shareezy.MAIL_SMTP_USER</dt>
-	 * <dd>mail.smtp.user - Name des Benutzers am SMTP-Server</dd>
+	 * <dd>mail.smtp.user - Name des Benutzers am SMTP-Server (wird nur
+	 * beachtet, wenn org.shareezy.MAIL_SMTP_AUTH=true)</dd>
 	 * <dt>org.shareezy.MAIL_SMTP_WORD</dt>
-	 * <dd>mail.smtp.word - Kennwort des Benutzers am SMTP-Server</dd>
+	 * <dd>mail.smtp.word - Kennwort des Benutzers am SMTP-Server (wird nur
+	 * beachtet, wenn org.shareezy.MAIL_SMTP_AUTH=true)</dd>
 	 * </dl>
 	 * 
-	 * Das Kontext-Attribut kann innerhalb der Applikation genutzt werden,
-	 * um E-Mails mit Hilfe javax.mail-APIs zu versenden und zu empfangen.
+	 * Das Kontext-Attribut kann innerhalb der Applikation genutzt werden, um
+	 * E-Mails mit Hilfe javax.mail-APIs zu versenden und zu empfangen.
 	 * 
 	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
 	 */
@@ -44,17 +48,24 @@ public class ContextListener implements ServletContextListener {
 		Properties ps = new Properties();
 		ServletContext sc = e.getServletContext();
 		ps.put("mail.from", sc.getInitParameter("org.shareezy.MAIL_FROM"));
-		ps.put("mail.smtp.auth",
-				Boolean.parseBoolean(sc.getInitParameter("org.shareezy.MAIL_SMTP_AUTH")));
+		ps.put("mail.smtp.host",
+				sc.getInitParameter("org.shareezy.MAIL_SMTP_HOST"));
+		ps.put("mail.smtp.port", Integer.parseInt(sc
+				.getInitParameter("org.shareezy.MAIL_SMTP_PORT")));
 		ps.put("mail.smtp.starttls.enable", Boolean.parseBoolean(sc
 				.getInitParameter("org.shareezy.MAIL_SMTP_STARTTLS_ENABLE")));
-		ps.put("mail.smtp.host", sc.getInitParameter("org.shareezy.MAIL_SMTP_HOST"));
-		ps.put("mail.smtp.port",
-				Integer.parseInt(sc.getInitParameter("org.shareezy.MAIL_SMTP_PORT")));
-		ps.put("mail.smtp.user", sc.getInitParameter("org.shareezy.MAIL_SMTP_USER"));
-		ps.put("mail.smtp.word", sc.getInitParameter("org.shareezy.MAIL_SMTP_WORD"));
-		sc.setAttribute("org.shareezy.MAIL_PROPERTIES", ps);
-		sc.log("SHAREEZY: Context Attribut 'org.shareezy.MAIL_PROPERTIES' hinzugefügt");
+
+		boolean smtpAuth = Boolean.parseBoolean(sc
+				.getInitParameter("org.shareezy.MAIL_SMTP_AUTH"));
+		ps.put("mail.smtp.auth", smtpAuth);
+		if (smtpAuth) {
+			ps.put("mail.smtp.user",
+					sc.getInitParameter("org.shareezy.MAIL_SMTP_USER"));
+			ps.put("mail.smtp.word",
+					sc.getInitParameter("org.shareezy.MAIL_SMTP_WORD"));
+			sc.setAttribute("org.shareezy.MAIL_PROPERTIES", ps);
+			sc.log("SHAREEZY: Context Attribut 'org.shareezy.MAIL_PROPERTIES' hinzugefügt");
+		}
 	}
 
 	/**
