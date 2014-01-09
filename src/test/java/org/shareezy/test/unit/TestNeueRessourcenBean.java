2e3da67ff3a8791be174cc008a7f1503f5ff0f31
timtreibmann <melusinesportal@yahoo.de>
2014-01-09 11:13:20 +0100

    #5 Code Formatierung verbessert und Leerzeichen entfernt

--- src/test/java/org/shareezy/test/unit/TestNeueRessourcenBean.java
+++ src/test/java/org/shareezy/test/unit/TestNeueRessourcenBean.java
@@ -18,19 +18,10 @@
 package org.shareezy.test.unit;
 
 import static org.mockito.Mockito.*;
-
 import java.lang.reflect.Field;
-
-
-
-
-
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -46,7 +37,6 @@ import org.shareezy.entities.Ressource;
  */
 
 public class TestNeueRessourcenBean {
-
 	private NeueRessourceBean proband;
 	@Mock
 	private EntityManagerFactory emf;
@@ -57,27 +47,20 @@ public class TestNeueRessourcenBean {
 	@Mock
 	private Ressource ressource;
 
-
-
-	
-
-
 	/**
 	 * Die Annotation <code>Before</code> sorgt dafür das vor dem Test die
 	 * folgende Methode ausgeführt wird, damit der Test vorbereitet werden kann.
 	 * Außerßerdem wird ein NeureRessourceBean Objekt erstellt und proband
 	 * zugewiesen. Dieser Proband wird noch nciht genutzt.
 	 */
-	
 	@Before
 	public void aufbau() throws Exception {
-		//Hiermit wird sichergestellt, dass die @Mock Annotationen verfügbar sind.
+		// Hiermit wird sichergestellt, dass die @Mock Annotationen verfügbar
+		// sind.
 		MockitoAnnotations.initMocks(this);
-		
 		proband = new NeueRessourceBean();
 		when(emf.createEntityManager()).thenReturn(em);
 		when(em.getTransaction()).thenReturn(transaction);
-		
 		Class<? extends NeueRessourceBean> klasse = proband.getClass();
 		Field field = klasse.getDeclaredField("emf");
 		field.setAccessible(true);
@@ -87,7 +70,7 @@ public class TestNeueRessourcenBean {
 	/**
 	 * Die Annotation Test identifiziert die Methode als Test. Es wird getestet
 	 * ob ein Objekt aus der Datenbank gelösct wird.
-	 */	
+	 */
 	@Test
 	public void testLoescheRessource() {
 
@@ -97,18 +80,19 @@ public class TestNeueRessourcenBean {
 		verify(transaction).begin();
 		verify(em).merge(ressource);
 		verify(em).remove(any());
-		//2 Variante für Testing der remove Methode - Intention: Es soll sichergestellt werden, dass ein Ressourcenobjekt gelöscht wird.
-		//verify(em).remove(em.merge(ressource));
+		// 2 Variante für Testing der remove Methode - Intention: Es soll
+		// sichergestellt werden, dass ein Ressourcenobjekt gelöscht wird.
+		// verify(em).remove(em.merge(ressource));
 		verify(transaction).commit();
 		verify(em).close();
 	}
-	
+
 	@Test
 	public void testSpeichern() {
 		proband.speichern();
 		verify(emf).createEntityManager();
 		verify(em, times(2)).getTransaction();
 		verify(em).persist(any());
-		verify(em).close();		
+		verify(em).close();
 	}
 }
