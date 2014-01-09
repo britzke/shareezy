f889225a3b9a31bc2e7bf4d917418a9fba62ab99
e1_hermann <e1_hermann@NI-LAB10>
2014-01-09 11:20:42 +0100

    #7 bearbeitet

--- src/main/java/org/shareezy/beans/GruppenzuordnungBean.java
+++ src/main/java/org/shareezy/beans/GruppenzuordnungBean.java
@@ -1,99 +1,94 @@
-/*
- * This file is part of shareezy, a software system for sharing resources.
- *
- * Copyright (C) 2013  	e1_herrmann
- * 						burghard.britzke (bubi@charmides.in-berlin.de)
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/**
+*
+*/
 package org.shareezy.beans;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.SessionScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import org.junit.Before;
+import org.shareezy.beans.GruppenzuordnungBean;
+import org.shareezy.entities.Gruppe;
 
 /**
- * Bean für die Gruppenzuordnung. Es wird geprüft welche Mitglieder Ressourcen
- * innerhalb der Gruppe verwalten können. Abfrage des Status der Ressource Wenn
- * jemand aus der Gruppe entfernt wird/geht. Welche Ressource zur Gruppe
- * hinzugefügt wird. Nachträgliches bearbeiten der ressourcen zu gruppe/ID.
- * Ressource der gruppe hinzufügen.
- * 
- * @author e1_hermann
- * @author burghard.britzke bubi@charmides.in-berlin.de
- **/
+* @author e1_hermann
+*
+*/
 @ManagedBean
 @SessionScoped
 public class GruppenzuordnungBean {
+	
 
-	public String gruppenersteller;
-	public String leitunguebertragen;
-	public String mitglied;
+	
+	public GruppenzuordnungBean proband;
 	public String account_id;
 	public String accounts_id;
-	public EntityManagerFactory emf;
-	public EntityManager em;
-
+    public EntityManagerFactory emf;
 
 	/**
-	 * Welches Mitglied ist berechtigt zum abfragen/erstellen/verwalten von
-	 * Ressourcen
-	 * 
-	 * @return accountID
+	 * @throws java.lang.Exception
 	 */
+	@Before
+	public void setUp() throws Exception {
+		/*
+	    Gruppenzuordnung i; 
+		i=new Gruppenzuordnung();
+		*/
+		proband = new GruppenzuordnungBean();
+	   
+		
+	}
+
+       
+
+	/**
+	 * Welches Mitglied ist berechtigt zum abfragen/erstellen/verwalten von Ressourcen
+	 * @return 
+	 */
+	
 	public String mitgliederabfragen() {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction t = em.getTransaction();
+		t.begin();
+		Gruppe gruppe = new Gruppe();
+        em.persist(gruppe);
 		return account_id;
-
+	
 	}
 
-	/**
-	 * Abfrage des status der ressource
-	 * 
-	 * @return
-	 */
-	public String ressourcestatus() {
+        /**
+         * Abfrage des Status der Ressource
+         */
+       
+        public String ressourcestatus() {
+        	return accounts_id;
+                
+        }
 
-		return accounts_id;
+        /**
+         * Welches Mitglied ist berechtigt zum abfragen/erstellen/verwalten von Ressourcen
+         */
+        public void mitgliedentfernen() {
+        }
 
-	}
+        /**
+         * Ressource zur gruppe hinzufügen. Erstmal aus der View abfragen, dann Datenbankabfrage
+         */
+        public void addressourcen(int ressourcenid) {
+        	return;
+               
+        }
 
-	/**
-	 * Mitglied aus der Gruppe entfernen/gruppenID
-	 * 
-	 * @return
-	 */
-	public String mitgliedentfernen() {
-		return null;
-	}
-
-	/**
-	 * Ressource zur gruppe hinzufügen. Erstmal aus der View abfragen, dann
-	 * Datenbankabfrage
-	 * 
-	 * @return
-	 */
-	public String addressourcen(int ressourcenid) {
-		return null;
-	}
-
-	/**
-	 * bearbeiten der ressource
-	 * 
-	 * @return
-	 */
-	public String editressource() {
-		return null;
-	}
+        /**
+         * Bearbeiten der Ressource
+         */
+        public void editressource() {
+        	return;
+               
+        }
 
 }
+
--- src/test/java/org/shareezy/test/unit/GruppenzuordnungBeanTest.java
+++ src/test/java/org/shareezy/test/unit/GruppenzuordnungBeanTest.java
@@ -1,433 +1,85 @@
-/*
- * This file is part of shareezy, a software system for sharing resources.
- *
- * Copyright (C) 2013  	e1_herrmann
- * 						burghard.britzke (bubi@charmides.in-berlin.de)
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * 
  */
 package org.shareezy.test.unit;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Field;
 
-import javax.persistence.Cache;
-import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.FlushModeType;
-import javax.persistence.LockModeType;
-import javax.persistence.PersistenceUnitUtil;
-import javax.persistence.Query;
-import javax.persistence.StoredProcedureQuery;
-import javax.persistence.SynchronizationType;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaDelete;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.CriteriaUpdate;
-import javax.persistence.metamodel.Metamodel;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.shareezy.beans.GroupManagerBean;
 import org.shareezy.beans.GruppenzuordnungBean;
 
 /**
- * Testet die GruppenzuordnungsBean.
- * 
  * @author e1_hermann
- * @author burghard.britzke (bubi@charmides.in-berlin.de)
+ *
  */
 public class GruppenzuordnungBeanTest {
-
+	
+	public EntityManager em;
+	public EntityManagerFactory emf;
 	public GruppenzuordnungBean proband;
-	public String account_id;
-	public String accounts_id;
+	public EntityTransaction transaction;
+
+	
 
 	/**
-	 * Attrappe für ein EntityManager-Objekt zum Testen der
-	 * GruppenzuordnungBean.
-	 * 
-	 * @author britzke
-	 */
-	public class MockEntityManager implements EntityManager {
-		public EntityManager createEntityManager() {
-			return new MockEntityManager();
-		}
-
-		@Override
-		public void clear() {
-		}
-
-		@Override
-		public void close() {
-		}
-
-		@Override
-		public boolean contains(Object arg0) {
-			return false;
-		}
-
-		@Override
-		public <T> EntityGraph<T> createEntityGraph(Class<T> arg0) {
-			return null;
-		}
-
-		@Override
-		public EntityGraph<?> createEntityGraph(String arg0) {
-			return null;
-		}
-
-		@Override
-		public Query createNamedQuery(String arg0) {
-			return null;
-		}
-
-		@Override
-		public <T> TypedQuery<T> createNamedQuery(String arg0, Class<T> arg1) {
-			return null;
-		}
-
-		@Override
-		public StoredProcedureQuery createNamedStoredProcedureQuery(String arg0) {
-			return null;
-		}
-
-		@Override
-		public Query createNativeQuery(String arg0) {
-			return null;
-		}
-
-		@Override
-		public Query createNativeQuery(String arg0, Class arg1) {
-			return null;
-		}
-
-		@Override
-		public Query createNativeQuery(String arg0, String arg1) {
-			return null;
-		}
-
-		@Override
-		public Query createQuery(String arg0) {
-			return null;
-		}
-
-		@Override
-		public <T> TypedQuery<T> createQuery(CriteriaQuery<T> arg0) {
-			return null;
-		}
-
-		@Override
-		public Query createQuery(CriteriaUpdate arg0) {
-			return null;
-		}
-
-		@Override
-		public Query createQuery(CriteriaDelete arg0) {
-			return null;
-		}
-
-		@Override
-		public <T> TypedQuery<T> createQuery(String arg0, Class<T> arg1) {
-			return null;
-		}
-
-		@Override
-		public StoredProcedureQuery createStoredProcedureQuery(String arg0) {
-			return null;
-		}
-
-		@Override
-		public StoredProcedureQuery createStoredProcedureQuery(String arg0,
-				Class... arg1) {
-			return null;
-		}
-
-		@Override
-		public StoredProcedureQuery createStoredProcedureQuery(String arg0,
-				String... arg1) {
-			return null;
-		}
-
-		@Override
-		public void detach(Object arg0) {
-		}
-
-		@Override
-		public <T> T find(Class<T> arg0, Object arg1) {
-			return null;
-		}
-
-		@Override
-		public <T> T find(Class<T> arg0, Object arg1, Map<String, Object> arg2) {
-			return null;
-		}
-
-		@Override
-		public <T> T find(Class<T> arg0, Object arg1, LockModeType arg2) {
-			return null;
-		}
-
-		@Override
-		public <T> T find(Class<T> arg0, Object arg1, LockModeType arg2,
-				Map<String, Object> arg3) {
-			return null;
-		}
-
-		@Override
-		public void flush() {
-		}
-
-		@Override
-		public CriteriaBuilder getCriteriaBuilder() {
-			return null;
-		}
-
-		@Override
-		public Object getDelegate() {
-			return null;
-		}
-
-		@Override
-		public EntityGraph<?> getEntityGraph(String arg0) {
-			return null;
-		}
-
-		@Override
-		public <T> List<EntityGraph<? super T>> getEntityGraphs(Class<T> arg0) {
-			return null;
-		}
-
-		@Override
-		public EntityManagerFactory getEntityManagerFactory() {
-			return null;
-		}
-
-		@Override
-		public FlushModeType getFlushMode() {
-			return null;
-		}
-
-		@Override
-		public LockModeType getLockMode(Object arg0) {
-			return null;
-		}
-
-		@Override
-		public Metamodel getMetamodel() {
-			return null;
-		}
-
-		@Override
-		public Map<String, Object> getProperties() {
-			return null;
-		}
-
-		@Override
-		public <T> T getReference(Class<T> arg0, Object arg1) {
-			return null;
-		}
-
-		@Override
-		public EntityTransaction getTransaction() {
-			return null;
-		}
-
-		@Override
-		public boolean isJoinedToTransaction() {
-			return false;
-		}
-
-		@Override
-		public boolean isOpen() {
-			return false;
-		}
-
-		@Override
-		public void joinTransaction() {
-		}
-
-		@Override
-		public void lock(Object arg0, LockModeType arg1) {
-		}
-
-		@Override
-		public void lock(Object arg0, LockModeType arg1,
-				Map<String, Object> arg2) {
-		}
-
-		@Override
-		public <T> T merge(T arg0) {
-			return null;
-		}
-
-		@Override
-		public void persist(Object arg0) {
-		}
-
-		@Override
-		public void refresh(Object arg0) {
-		}
-
-		@Override
-		public void refresh(Object arg0, Map<String, Object> arg1) {
-		}
-
-		@Override
-		public void refresh(Object arg0, LockModeType arg1) {
-		}
-
-		@Override
-		public void refresh(Object arg0, LockModeType arg1,
-				Map<String, Object> arg2) {
-		}
-
-		@Override
-		public void remove(Object arg0) {
-		}
-
-		@Override
-		public void setFlushMode(FlushModeType arg0) {
-		}
-
-		@Override
-		public void setProperty(String arg0, Object arg1) {
-		}
-
-		@Override
-		public <T> T unwrap(Class<T> arg0) {
-			return null;
-		}
-	}
-
-	/**
-	 * Attrappe für eine EntityManagerFactory zum Testen der
-	 * GruppenzuordnungsBean.
-	 * 
-	 * @author e1_herrmann
-	 * @author britzke
-	 */
-	public class MockEntityManagerFactory implements EntityManagerFactory {
-
-		@Override
-		public EntityManager createEntityManager() {
-			return null;
-		}
-
-		@Override
-		public EntityManager createEntityManager(Map map) {
-			return null;
-		}
-
-		@Override
-		public EntityManager createEntityManager(
-				SynchronizationType synchronizationType) {
-			return null;
-		}
-
-		@Override
-		public EntityManager createEntityManager(
-				SynchronizationType synchronizationType, Map map) {
-			return null;
-		}
-
-		@Override
-		public CriteriaBuilder getCriteriaBuilder() {
-			return null;
-		}
-
-		@Override
-		public Metamodel getMetamodel() {
-			return null;
-		}
-
-		@Override
-		public boolean isOpen() {
-			return false;
-		}
-
-		@Override
-		public void close() {
-		}
-
-		@Override
-		public Map<String, Object> getProperties() {
-			return null;
-		}
-
-		@Override
-		public Cache getCache() {
-			return null;
-		}
-
-		@Override
-		public PersistenceUnitUtil getPersistenceUnitUtil() {
-			return null;
-		}
-
-		@Override
-		public void addNamedQuery(String name, Query query) {
-
-		}
-
-		@Override
-		public <T> T unwrap(Class<T> cls) {
-			return null;
-		}
-
-		@Override
-		public <T> void addNamedEntityGraph(String graphName,
-				EntityGraph<T> entityGraph) {
-		}
-	}
-
-	/**
-	 * Initalisierung der Testumgebung für alle Tests der GruppenzuordnungBean.
-	 * 
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
 		proband = new GruppenzuordnungBean();
+		emf=mock(EntityManagerFactory.class);
+		em=mock(EntityManager.class);
+		when(emf.createEntityManager()).thenReturn(em);
+		when(em.getTransaction()).thenReturn(transaction);
+		
 	}
-
+	
+	Class<? extends GruppenzuordnungBean> clazz = proband.getClass();
+	/*
+    Field field = clazz.getDeclaredField("emf");
+    
+    field.setAccessible(true);
+    field.set(proband, emf);
+    */
 	/**
-	 * Test method for
-	 * {@link org.shareezy.beans.Gruppenzuordnung#mitgliederabfragen()}.
+	 * Test method for {@link org.shareezy.beans.GruppenzuordnungBean#setUp()}.
 	 */
 	@Test
-	public void testmitgliederabfragen() {
-		String a = proband.mitgliederabfragen();
-		assertEquals(account_id, a);
+	public void testSetUp() {
+		fail("Not yet implemented");
 	}
 
 	/**
-	 * Test method for
-	 * {@link org.shareezy.beans.Gruppenzuordnung#ressourcestatus()}.
+	 * Test method for {@link org.shareezy.beans.GruppenzuordnungBean#mitgliederabfragen()}.
+	 */
+	@Test
+	public void testMitgliederabfragen() {
+		verify(emf).createEntityManager();
+		verify(em).getTransaction();
+		verify(transaction).begin();
+		verify(em).persist(any());
+	}
+
+	/**
+	 * Test method for {@link org.shareezy.beans.GruppenzuordnungBean#ressourcestatus()}.
 	 */
 	@Test
 	public void testRessourcestatus() {
-		String b = proband.mitgliederabfragen();
-		assertEquals(accounts_id, b);
+		fail("Not yet implemented");
 	}
 
 	/**
-	 * Test method for
-	 * {@link org.shareezy.beans.Gruppenzuordnung#mitgliedentfernen()}.
+	 * Test method for {@link org.shareezy.beans.GruppenzuordnungBean#mitgliedentfernen()}.
 	 */
 	@Test
 	public void testMitgliedentfernen() {
@@ -435,8 +87,7 @@ public class GruppenzuordnungBeanTest {
 	}
 
 	/**
-	 * Test method for
-	 * {@link org.shareezy.beans.Gruppenzuordnung#addressourcen(int)}.
+	 * Test method for {@link org.shareezy.beans.GruppenzuordnungBean#addressourcen(int)}.
 	 */
 	@Test
 	public void testAddressourcen() {
@@ -444,11 +95,11 @@ public class GruppenzuordnungBeanTest {
 	}
 
 	/**
-	 * Test method for
-	 * {@link org.shareezy.beans.Gruppenzuordnung#editressource()}.
+	 * Test method for {@link org.shareezy.beans.GruppenzuordnungBean#editressource()}.
 	 */
 	@Test
 	public void testEditressource() {
 		fail("Not yet implemented");
 	}
+
 }
