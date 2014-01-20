586069d90c9aab4cd109cfa2cc457bea8293e861
burghard.britzke <bubi@charmides.in-berlin.de>
2014-01-20 11:12:44 +0100

    #10 Dokumentiert, formatiert, ArgumentMatcher korrigiert

--- src/test/java/org/shareezy/test/unit/GroupMemberManagerBeanTest.java
+++ src/test/java/org/shareezy/test/unit/GroupMemberManagerBeanTest.java
@@ -1,7 +1,9 @@
 /*
  * This file is part of shareezy, a software system for sharing resources.
  *
- * Copyright (C) 2013  	Timo Kuchling
+ * Copyright (C) 2013, 2014
+ * 						Timo Kuchling
+ * 						burghard.britzke bubi@charmides.in-berlin.de
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -17,47 +19,31 @@
  */
 package org.shareezy.test.unit;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Field;
-import java.util.List;
-import java.util.Map;
 
-import javax.persistence.Cache;
-import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.FlushModeType;
-import javax.persistence.LockModeType;
-import javax.persistence.PersistenceUnitUtil;
-import javax.persistence.Query;
-import javax.persistence.StoredProcedureQuery;
-import javax.persistence.SynchronizationType;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaDelete;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.CriteriaUpdate;
-import javax.persistence.metamodel.Metamodel;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatcher;
-import org.mockito.Matchers;
 import org.mockito.Mockito;
-import org.shareezy.beans.GroupManagerBean;
 import org.shareezy.beans.GroupMemberManagerBean;
 import org.shareezy.entities.Benutzer;
 
+/**
+ * Testet die GroupMemberManagerBean
+ * 
+ * @author Timo Kuchling
+ * @author burghard.britzke
+ */
 public class GroupMemberManagerBeanTest {
 
 	private GroupMemberManagerBean proband;
@@ -66,25 +52,35 @@ public class GroupMemberManagerBeanTest {
 	private EntityTransaction transaction;
 	private Benutzer user;
 	private String nullTest = null;
+	private int testBenutzerHash;
 
 	/**
-	 * Setzt den Probanden auf.
+	 * Setzt den Probanden und die Testumgebung auf.
 	 * 
-	 * @throws java.lang.Exception
+	 * @throws SecurityException
+	 *             Wenn ein Security-Manager das Reflection verhindert
+	 * @throws NoSuchFieldException
+	 *             Wenn der Proband keine Eigenschaft "emf" hat
+	 * @throws IllegalAccessException
+	 *             Wenn nicht auf die Eigenschaft zugegriffen werden darf
+	 *             (setAccessible(true) fehlt - Fehler im Test)
+	 * @throws IllegalArgumentException
+	 *             Wenn die Eigenschaft in ein anderes als das Probanden-Objekt
+	 *             injiziert wird (Fehler im Test)
 	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws NoSuchFieldException, SecurityException,
+			IllegalArgumentException, IllegalAccessException {
 		proband = new GroupMemberManagerBean();
-		user = mock(Benutzer.class);
-		
+		user = new Benutzer();
+		// Sichere Objekt Identität für den BenutzerArgumentMatcher
+		testBenutzerHash = user.hashCode();
+
 		emf = mock(EntityManagerFactory.class);
-
 		em = mock(EntityManager.class);
-
 		when(emf.createEntityManager()).thenReturn(em);
 
 		transaction = mock(EntityTransaction.class);
-
 		when(em.getTransaction()).thenReturn(transaction);
 
 		// Beschreibung der Klasse holen
@@ -95,22 +91,22 @@ public class GroupMemberManagerBeanTest {
 		field.setAccessible(true);
 		// EntityManagerFactory in den Proband inizieren
 		field.set(proband, emf);
-		
+
 		field = clazz.getDeclaredField("user");
 		field.setAccessible(true);
-		field.set(proband, user );
+		field.set(proband, user);
 	}
 
 	/**
 	 * Test method for
-	 * {@link org.shareezy.beans.GroupMemberManagerBean#addUser()}.
-	 * Testet, ob ein Datensatz eingefügt wird.
+	 * {@link org.shareezy.beans.GroupMemberManagerBean#addUser()}. Testet, ob
+	 * ein Datensatz eingefügt wird.
 	 */
 	@Test
 	public void testAddUser() {
 		String antwort = proband.addUser();
 		assertNull("Die Antwort muss null sein", antwort);
-		
+
 		verify(emf).createEntityManager();
 		verify(em).getTransaction();
 		verify(transaction).begin();
@@ -118,34 +114,49 @@ public class GroupMemberManagerBeanTest {
 		verify(transaction).commit();
 		verify(em).close();
 	}
-	class MessagesArgumentMatcher extends ArgumentMatcher {
-		String userKurzname = "test";
-		GroupMemberManagerBean testProband = proband;
-		Benutzer testUser = testProband.getUser();
-		
-		         public boolean matches(Object o) {
-		        	 if(testUser.getKurzname() == userKurzname){
-		        		 System.out.println("UserKurzname ist 'test'");
-		        		 return true;
-		        	 }
-		             return false;
-		         }
-		     }
+
+	/**
+	 * Der BenutzerArgumentMatcher ist ein ArgumentMatcher, der sicherstellt, ob
+	 * ein Benutzer als Argument geliefert wurde, dessen hashCode dem Kode
+	 * entspricht, der in der Variablen 'testBenutzerHash' gespeichert ist.
+	 * 
+	 * @author Timo Kuchling
+	 * @author burghard.britzke bubi@charmides.in-berlin.de
+	 * @see ArgumentMatcher
+	 */
+	class BenutzerArgumentMatcher extends ArgumentMatcher<Benutzer> {
+
+		/**
+		 * Stellt sicher, dass als Argument ein Benutzer mit dem hashCode
+		 * übergeben wurde, der in der variablen "testBenutzerHash gespeichert
+		 * wurde.
+		 * 
+		 * @see ArgumentMatcher#matches(Object)
+		 */
+		public boolean matches(Object argument) {
+			if (argument instanceof Benutzer
+					&& (argument.hashCode() == testBenutzerHash)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
 
 	/**
 	 * Test method for
-	 * {@link org.shareezy.beans.GroupMemberManagerBean#deleteUser()}.
-	 * Testet, ob ein Datensatz gelöscht wird.
+	 * {@link org.shareezy.beans.GroupMemberManagerBean#deleteUser()}. Testet,
+	 * ob ein Datensatz gelöscht wird.
 	 */
 	@Test
 	public void testDeleteUser() {
 		String antwort = proband.deleteUser();
 		assertNull("Die Antwort muss null sein", antwort);
-		
+
 		verify(emf).createEntityManager();
 		verify(em).getTransaction();
 		verify(transaction).begin();
-		verify(em).remove(Mockito.argThat(new MessagesArgumentMatcher()));
+		verify(em).remove(Mockito.argThat(new BenutzerArgumentMatcher()));
 		verify(transaction).commit();
 		verify(em).close();
 	}
@@ -153,18 +164,18 @@ public class GroupMemberManagerBeanTest {
 	/**
 	 * Test method for
 	 * {@link org.shareezy.beans.GroupMemberManagerBean#deleteRequest()}.
-	 * Testet, ob ein Request gelöscht wird.
+	 * Testet, ob eine Anfrage für eine Gruppenmitgliedschaft gelöscht wird.
 	 */
 	@Test
 	public void testDeleteRequest() {
 		String antwort = proband.deleteRequest();
 		assertNull(nullTest, antwort);
 	}
-	
+
 	/**
 	 * Test method for
-	 * {@link org.shareezy.beans.GroupMemberManagerBean#sendRequest()}.
-	 * Testet, ob ein Request gesendet wird.
+	 * {@link org.shareezy.beans.GroupMemberManagerBean#sendRequest()}. Testet,
+	 * ob ein Request gesendet wird.
 	 */
 	@Test
 	public void testSendRequest() {
