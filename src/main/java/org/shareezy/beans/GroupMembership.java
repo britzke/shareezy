208339aac6eaf62c4b08c2e1e2178f1f4a50124f
mackito <mackito@mail.ru>
2014-01-28 10:08:47 +0100

    #6 gelöscht "dashboardcolumn"

--- src/main/java/org/shareezy/beans/GroupMembership.java
+++ src/main/java/org/shareezy/beans/GroupMembership.java
@@ -47,47 +47,6 @@ public class GroupMembership implements Serializable{
 	private Benutzer benutzer;
 	private boolean bestätigt;
 	private boolean administrator;
-	private DashboardModel model;  
-	
-	public GroupMembership(){		
-		/**
-		 * GruppenListe mit Button "Der Community beitreten" 
-		 * und "Die Community verlassen"
-		 * */
-		model = new DefaultDashboardModel();  
-        DashboardColumn column1 = new DefaultDashboardColumn();  
-        DashboardColumn column2 = new DefaultDashboardColumn();  
-        DashboardColumn column3 = new DefaultDashboardColumn();  
-          
-        column1.addWidget("sports");  
-        column1.addWidget("finance");  
-          
-        column2.addWidget("lifestyle");  
-        column2.addWidget("weather");  
-          
-        column3.addWidget("politics");  
-  
-        model.addColumn(column1);  
-        model.addColumn(column2);  
-        model.addColumn(column3); 
-	}
-	 public void handleReorder(DashboardReorderEvent event) {  
-	        FacesMessage message = new FacesMessage();  
-	        message.setSeverity(FacesMessage.SEVERITY_INFO);  
-	        message.setSummary("Reordered: " + event.getWidgetId());  
-	        message.setDetail("Item index: " + event.getItemIndex() + ", Column index: " + event.getColumnIndex() + ", Sender index: " + event.getSenderColumnIndex());  
-	          
-	        addMessage(message);  
-	    }        
-	      
-	    private void addMessage(FacesMessage message) {  
-	        FacesContext.getCurrentInstance().addMessage(null, message);  
-	    }  
-	      
-	    public DashboardModel getModel() {  
-	        return model;  
-	    }  
-	    
 	
 	public String GroupMembership(){
 
