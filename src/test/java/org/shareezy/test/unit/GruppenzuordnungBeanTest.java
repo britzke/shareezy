4d83d934d1f238019a4c76c9a8bc241ccb46636e
britzke <britzke@lette-verein.de>
2014-01-30 11:05:10 +0100

    GPL Header eingefügt überschriebene Javadoc wieder eingefügt

--- src/test/java/org/shareezy/test/unit/GruppenzuordnungBeanTest.java
+++ src/test/java/org/shareezy/test/unit/GruppenzuordnungBeanTest.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of shareezy, a software system for sharing resources.
+ *
+ * Copyright (C) 2013  	e1_herrmann
+ * 						burghard.britzke (bubi@charmides.in-berlin.de)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.shareezy.test.unit;
 
 import static org.junit.Assert.*;
@@ -14,8 +32,10 @@ import org.junit.Test;
 import org.shareezy.beans.GruppenzuordnungBean;
 
 /**
- * @author e1_hermann
+ * Testet die GruppenzuordnungsBean.
  * 
+ * @author e1_hermann
+ * @author burghard.britzke (bubi@charmides.in-berlin.de)
  */
 public class GruppenzuordnungBeanTest {
 
@@ -25,6 +45,8 @@ public class GruppenzuordnungBeanTest {
 	private Query query;
 
 	/**
+	 * Initalisierung der Testumgebung für alle Tests der GruppenzuordnungBean.
+	 * 
 	 * @throws java.lang.Exception
 	 */
 	@Before
